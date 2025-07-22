c8b70475e52221ad000f9132ce84f8073bc52560
Kavitha Pandiyan <kavithapazuredataengg@gmail.com>
2025-07-22 13:06:35 +0530

    Add files via upload

---
+++ sqldb_query.sql
@@ -0,0 +1,16 @@
+CREATE TABLE source_data_car_proj
+(
+Branch_ID varchar(200),
+Dealer_ID varchar(200),
+Model_ID varchar(200), 
+Revenue BIGINT,
+Units_Sold BIGINT,
+Date_ID varchar(200),
+Day	INT,
+Month INT,
+Year INT,	
+BranchName	varchar(2000),
+DealerName	varchar(2000)
+)
+
+select * from source_data_car_proj
